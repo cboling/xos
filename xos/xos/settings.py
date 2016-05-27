3b3a54a461b5879c0d7de731a1e94734c29fbac2
Chip Boling <chip@bcsw.net>
2016-05-27 17:45:27 -0500

    adding vaos back in

--- xos/xos/settings.py
+++ xos/xos/settings.py
@@ -195,7 +195,7 @@ INSTALLED_APPS = (
     'services.vrouter',
     'services.vtn',
     'services.fabric',
-    # 'services.vaos',                    # ADTN: Added vAOS service
+    'services.vaos',                    # ADTN: Added vAOS service
     'geoposition',
     'rest_framework_swagger',
 )
