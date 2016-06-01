80a43bd0acd81f71679bda2f47bee9baf9bfee7e
Chip Boling <chip@bcsw.net>
2016-06-01 12:19:02 -0500

    Remove unused (incorrect) imported class

--- xos/core/xoslib/methods/vaostenant.py
+++ xos/core/xoslib/methods/vaostenant.py
@@ -6,7 +6,7 @@ from rest_framework import generics
 from rest_framework import status
 from core.models import *
 from django.forms import widgets
-from services.vaosservice.models import VaosTenant, VaosService, CordSubscriberRoot
+from services.vaosservice.models import VaosTenant, VaosService
 from plus import PlusSerializerMixin
 from xos.apibase import XOSListCreateAPIView, XOSRetrieveUpdateDestroyAPIView, XOSPermissionDenied
 
