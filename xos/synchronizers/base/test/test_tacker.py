0309e543cfa712775f9d9c53c5627b5c9d43d499
cboling <Painter456>
2016-06-19 15:44:14 -0500

    Unit test cli parameters

--- xos/synchronizers/base/test/test_tacker.py
+++ xos/synchronizers/base/test/test_tacker.py
@@ -8,25 +8,32 @@ from tackerclient.client import HTTPClient, construct_http_client
 from tackerclient.common.exceptions import Unauthorized, ConnectionFailed
 from tackerclient.common.exceptions import SslCertificateValidationError
 from requests.exceptions import HTTPError
+import argparse
+
+# These credentials from 'defaults.py' can be assigned on the command line via argparse
+_test_credentials = {
+    'user': OPENSTACK_USER,
+    'password': OPENSTACK_PASSWORD,
+    'tenant': OPENSTACK_TENANT_NAME,
+    'auth_url': OPENSTACK_AUTH_URL,
+    'service_type': OPENSTACK_SERVICE_TYPE
+}
 
 
-def _create_site(user = OPENSTACK_USER,
-                 password = OPENSTACK_PASSWORD,
-                 tenant = OPENSTACK_TENANT_NAME,
-                 auth_url = OPENSTACK_AUTH_URL):
-    return SiteTest(user, password, tenant, auth_url)
+def _create_site(user=OPENSTACK_USER, password=OPENSTACK_PASSWORD,
+                 tenant=OPENSTACK_TENANT_NAME, auth_url=OPENSTACK_AUTH_URL):
+    return SiteTest(user=user,
+                    password=password,
+                    tenant=tenant,
+                    auth_url=auth_url)
 
 
-def _create_client(user = OPENSTACK_USER,
-                   password = OPENSTACK_PASSWORD,
-                   tenant = OPENSTACK_TENANT_NAME,
-                   auth_url = OPENSTACK_AUTH_URL,
-                   service_type = OPENSTACK_SERVICE_TYPE):
-    return tacker.construct_http_client(_create_site(user=user,
-                                                     password=password,
-                                                     tenant=tenant,
-                                                     auth_url=auth_url),
-                                        service_type=service_type)
+def _create_client(credentials=_test_credentials):
+    return tacker.construct_http_client(user=credentials['user'],
+                                        password=credentials['password'],
+                                        tenant=credentials['tenant'],
+                                        auth_url=credentials['auth_url'],
+                                        service_type=credentials['service_type'])
 
 
 def _onboard_vnfd(client, vnfd_file):
@@ -228,4 +235,29 @@ class VNFDestroyTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Tacker Unit Test')
+
+    parser.add_argument('--verbose', '-v', action='store_true', default=False,
+                        help='Output verbose information')
+    parser.add_argument('--username', '-u', action='store', default=OPENSTACK_USER,
+                        help='Administrative User Name')
+    parser.add_argument('--password', '-p', action='store', default=OPENSTACK_PASSWORD,
+                        help='Administrative Password')
+    parser.add_argument('--tenant', '-t', action='store', default=OPENSTACK_TENANT_NAME,
+                        help='Administrative Tenant Name')
+    parser.add_argument('--auth_url', '-a', action='store', default=OPENSTACK_AUTH_URL,
+                        help='Keystone Authorization URL')
+    parser.add_argument('--service_type', '-s', action='store', default=OPENSTACK_SERVICE_TYPE,
+                        help='Service Type for Tacker')
+    args = parser.parse_args()
+
+    _test_credentials = {
+        'user': args.username,
+        'password': args.password,
+        'tenant': args.tenant,
+        'auth_url': args.auth_url,
+        'service_type': args.service_type
+    }
+    # Run the tests
+
     unittest.main()
